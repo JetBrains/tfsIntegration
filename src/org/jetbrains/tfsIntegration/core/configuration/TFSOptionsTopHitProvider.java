c344aedf05c84e6b2049153a45f008cba36d724d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-02-23 22:05:53 +0100

    OptionsTopHitProvider — do not load project extensions for app level to avoid unrelated classes loading
    Also makes user implementations more clear (no need to check is project null or not)

--- src/org/jetbrains/tfsIntegration/core/configuration/TFSOptionsTopHitProvider.java
+++ src/org/jetbrains/tfsIntegration/core/configuration/TFSOptionsTopHitProvider.java
@@ -1,6 +1,6 @@
 package org.jetbrains.tfsIntegration.core.configuration;
 
-import com.intellij.ide.ui.OptionsTopHitProvider;
+import com.intellij.ide.ui.OptionsSearchTopHitProvider;
 import com.intellij.ide.ui.PublicMethodBasedOptionDescription;
 import com.intellij.ide.ui.search.BooleanOptionDescription;
 import com.intellij.ide.ui.search.OptionDescription;
@@ -8,7 +8,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.openapi.vcs.impl.VcsDescriptor;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
@@ -18,7 +17,7 @@ import java.util.Collections;
 /**
  * @author Sergey.Malenkov
  */
-public final class TFSOptionsTopHitProvider extends OptionsTopHitProvider {
+final class TFSOptionsTopHitProvider implements OptionsSearchTopHitProvider.ProjectLevelProvider {
   @Override
   public String getId() {
     return "vcs";
@@ -26,16 +25,14 @@ public final class TFSOptionsTopHitProvider extends OptionsTopHitProvider {
 
   @NotNull
   @Override
-  public Collection<OptionDescription> getOptions(@Nullable Project project) {
-    if (project != null) {
-      for (VcsDescriptor descriptor : ProjectLevelVcsManager.getInstance(project).getAllVcss()) {
-        if ("TFS".equals(descriptor.getDisplayName())) {
-          return Collections.unmodifiableCollection(Arrays.<BooleanOptionDescription>asList(
-            new Option("TFS: Use HTTP Proxy settings", null, "useIdeaHttpProxy", "setUseIdeaHttpProxy"),
-            new Option("TFS: Evaluate Team Explorer policies", "teamExplorer", null, "setSupportTfsCheckinPolicies"),
-            new Option("TFS: Evaluate Teamprise policies", "teamprise", null, "setSupportStatefulCheckinPolicies"),
-            new Option("TFS: Warn about not installed policies", "nonInstalled", null, "setReportNotInstalledCheckinPolicies")));
-        }
+  public Collection<OptionDescription> getOptions(@NotNull Project project) {
+    for (VcsDescriptor descriptor : ProjectLevelVcsManager.getInstance(project).getAllVcss()) {
+      if ("TFS".equals(descriptor.getDisplayName())) {
+        return Collections.unmodifiableCollection(Arrays.<BooleanOptionDescription>asList(
+          new Option("TFS: Use HTTP Proxy settings", null, "useIdeaHttpProxy", "setUseIdeaHttpProxy"),
+          new Option("TFS: Evaluate Team Explorer policies", "teamExplorer", null, "setSupportTfsCheckinPolicies"),
+          new Option("TFS: Evaluate Teamprise policies", "teamprise", null, "setSupportStatefulCheckinPolicies"),
+          new Option("TFS: Warn about not installed policies", "nonInstalled", null, "setReportNotInstalledCheckinPolicies")));
       }
     }
     return Collections.emptyList();
